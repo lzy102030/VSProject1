88ed694699bc60efd132f784744c44b06ceb0268
None_id <qinyuelai128@yeah.net>
2021-07-20 12:25:39 +0800

    客户端网络部分完成

--- src/client/UI/Lobby/Choose.java
+++ src/client/UI/Lobby/Choose.java
@@ -1,11 +1,19 @@
 package client.UI.Lobby;
 
+import Server.Service.test;
+import client.Service.inGame.MyHeroPro;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
 
-public class Choose extends JFrame{
+import static java.lang.Thread.sleep;
+
+public class Choose extends JFrame {
     JPanel jp1 = new JPanel();
     JPanel jp2 = new JPanel();
     JPanel jp3 = new JPanel();
@@ -15,30 +23,106 @@ public class Choose extends JFrame{
     JButton b3 = new JButton();
     JButton b4 = new JButton();
 
+    static Socket connection;
+    static ObjectOutputStream serverOut;
+    static ObjectInputStream serverIn;
+    static MyHeroPro hero;
+    static String host = "127.0.0.1";
+    static int port = 2000;
+
     public static void main(String[] args) {
         Choose choose = new Choose();
         choose.launchFrame();
         choose.setVisible(true);
 
-        //网络
+        //网络部分
+        //get connect to the server
+        boolean connectFlag = false;
+        while (!connectFlag) {
+            try {
+                connection = new Socket(host, port);
+                connectFlag = true;
+            } catch (IOException e) {
+                //while connect failed, 10 sec for retry
+                System.err.println("[ERROR]Cannot connect to the server. 10 seconds to retry.");
+                connectFlag = false;
+                try {
+                    sleep(10000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        //remote thread start for checking new contents
+        Thread thread = new Thread(new Choose.RemoteReader());
+        thread.start();
+
+        //get the IO streams
+        try {
+            serverIn = new ObjectInputStream(connection.getInputStream());
+            serverOut = new ObjectOutputStream(connection.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("[ERROR]Cannot get object from the server.");
+            e.printStackTrace();
+        }
     }
 
-    public void launchFrame(){
+    private static class RemoteReader implements Runnable {
+        @Override
+        public void run() {
+            Object heroReceive = null;
+            try {
+                while (true) {
+                    //1号玩家英雄信息
+                    do {
+                        try {
+                            heroReceive = serverIn.readObject();
+                        } catch (ClassNotFoundException e) {
+                            System.err.println("None Object received from server.");
+                            e.printStackTrace();
+                        }
+                    } while (heroReceive == null);
+                    hero = (MyHeroPro) heroReceive;
+
+                    //[TODO]接下来显示等处理
+
+                    //2号玩家英雄信息
+                    do {
+                        try {
+                            heroReceive = serverIn.readObject();
+                        } catch (ClassNotFoundException e) {
+                            System.err.println("None Object received from server.");
+                            e.printStackTrace();
+                        }
+                    } while (heroReceive == null);
+                    hero = (MyHeroPro) heroReceive;
+
+                    //[TODO]接下来显示等处理
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void launchFrame() {
         this.add(jp1);
         this.add(jp2);
         this.add(jp3);
         this.add(jp4);
-        this.setSize(1000,800);
-        jp1.setBounds(0,0,1000,600);
+        this.setSize(1000, 800);
+        jp1.setBounds(0, 0, 1000, 600);
 
         ImageIcon icon1 = new ImageIcon("E:\\work\\JavaTerm\\VSProject\\src\\client\\source\\草薙京.jpg");
         ImageIcon icon2 = new ImageIcon("E:\\work\\JavaTerm\\VSProject\\src\\client\\source\\草薙京.jpg");
-        ImageIcon icon3 = new ImageIcon("E:\\work\\JavaTerm\\VSProject\\src\\client\\source\\草薙京.jpg");
+        ImageIcon icon3 = new ImageIcon(Objects.requireNonNull(
+                this.getClass().getResource("/client/source/草薙京.jpg")));
         ImageIcon icon4 = new ImageIcon("E:\\work\\JavaTerm\\VSProject\\src\\client\\source\\草薙京.jpg");
-        icon1.setImage(icon1.getImage().getScaledInstance(this.getWidth()/4,this.getHeight(),Image.SCALE_DEFAULT));
-        icon2.setImage(icon2.getImage().getScaledInstance(this.getWidth()/4,this.getHeight(),Image.SCALE_DEFAULT));
-        icon3.setImage(icon3.getImage().getScaledInstance(this.getWidth()/4,this.getHeight(),Image.SCALE_DEFAULT));
-        icon4.setImage(icon4.getImage().getScaledInstance(this.getWidth()/4,this.getHeight(),Image.SCALE_DEFAULT));
+        icon1.setImage(icon1.getImage().getScaledInstance(this.getWidth() / 4, this.getHeight(), Image.SCALE_DEFAULT));
+        icon2.setImage(icon2.getImage().getScaledInstance(this.getWidth() / 4, this.getHeight(), Image.SCALE_DEFAULT));
+        icon3.setImage(icon3.getImage().getScaledInstance(this.getWidth() / 4, this.getHeight(), Image.SCALE_DEFAULT));
+        icon4.setImage(icon4.getImage().getScaledInstance(this.getWidth() / 4, this.getHeight(), Image.SCALE_DEFAULT));
         b1.setIcon(icon1);
         b2.setIcon(icon2);
         b3.setIcon(icon3);
@@ -48,7 +132,7 @@ public class Choose extends JFrame{
         jp3.add(b3);
         jp4.add(b4);
 
-        this.setLayout(new GridLayout(1,4));
+        this.setLayout(new GridLayout(1, 4));
 
         b1.addActionListener(new ActionListener() {
             @Override
