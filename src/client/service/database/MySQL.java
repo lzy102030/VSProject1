79627f1e31037fed16b250a7a60104642ad795d8
Noneid <qinyuelai128@yeah.net>
2021-07-26 21:29:39 +0800

    mysql数据库网络连接

--- src/client/service/database/MySQL.java
+++ src/client/service/database/MySQL.java
@@ -4,11 +4,12 @@ import java.sql.*;
 
 public class MySQL {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/VSProject?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    static final String DB_URL = //"jdbc:mysql://localhost:3306/VSProject?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+            "jdbc:mysql://noneid.myds.me:3306/VSProject?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USER = "root";
-    static final String PASS = "123789456";
+    static final String PASS = "QylLzy2021VsP";
 
     public static void main(String[] args) {
         Connection conn = null;
