ebf9d497828d4aab821d6147af278f2a111dc89d
Noneid <qinyuelai128@yeah.net>
2021-07-26 16:20:41 +0800

    redo comment

---
+++ src/server/service/WinnerPending.java
@@ -0,0 +1,26 @@
+package server.service;
+
+public class WinnerPending {
+    private int hp1, hp2;
+
+    public WinnerPending() {
+
+    }
+
+    public WinnerPending(int hp1, int hp2) {
+        this.hp1 = hp1;
+        this.hp2 = hp2;
+    }
+
+    public int winPending() {
+        if (hp1 <= 0 && hp2 > 0) {
+            return 2; //2胜
+        } else if (hp1 > 0 && hp2 <= 0) {
+            return 1;  //1胜
+        } else if (hp1 <= 0 && hp2 <= 0) {
+            return 0;  //平
+        } else {
+            return -1;  //未结束
+        }
+    }
+}
